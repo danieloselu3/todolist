b29510648fdf6259ac13035e8341d42097bc255d
Daniel Oselu <doselu3@gmail.com>
2021-04-06 12:37:07 +0300

    added add view function logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,24 @@
+from django import forms
 from django.shortcuts import render
+
+tasks = ['foo', 'bar', 'baz']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label='NewTask')
 
 # Create your views here.
 
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        'tasks':tasks,
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            tasks.append(task)
+    return render(request, "tasks/add.html", {
+        'form':NewTaskForm,
+    })
